032b988bbbebfaffa5f88bbf34a1b14bf0df26db
mustafa <mustafagamal51112@gmail.com>
2023-09-06 23:55:50 +0300

    --myfix

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Serve the React frontend
 app.use(express.static(path.join(__dirname, 'client/dist')));
 app.get('*', (req, res) => {
@@ -13,10 +35,7 @@ app.get('*', (req, res) => {
 
 
 
-
-
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
